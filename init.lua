9c204b508e6a716e38023e439fdb36a83ebb9715
rubenwardy <anjayward@gmail.com>
2014-04-11 19:25:29 +0100

    Cupcakes and Muffins

--- init.lua
+++ init.lua
@@ -9,6 +9,10 @@
 
 print("Sweet Foods - Version 0.1")
 
+if not food or not food.version or food.version < 2.1 then
+	error("You need to update your version of the Food mod - it is too old")
+end
+
 -- Boilerplate to support localized strings if intllib mod is installed.
 local S
 if (minetest.get_modpath("intllib")) then
@@ -20,12 +24,12 @@ end
 
 food.support("strawberry","farming_plus","farming_plus:strawberry_item")
 food.support("strawberry","plantlib","plantlib:strawberry")
+food.support("strawberry","bushes_classic","bushes:strawberry")
 food.support("rhubarb","farming_plus","farming_plus:rhubarb_item")
 
 if minetest.setting_get("food_sweet_use_2d")==nil then
 	minetest.setting_set("food_sweet_use_2d", "1")
 end
-
 
 food.asupport("walnut",function()
 	minetest.register_craftitem("food_sweet:walnut", {
@@ -36,9 +40,9 @@ food.asupport("walnut",function()
 	food.craft({
 		output = "food_sweet:walnut 4",
 		recipe = {
-			{"","default:sand",""},
 			{"default:sand","","default:sand"},
-			{"","default:sand",""}
+			{"","default:sand",""},
+			{"default:sand","","default:sand"}
 		}
 	})
 end)
@@ -79,11 +83,26 @@ food.asupport("rhubarb",function()
 		groups = {food_rhubarb = 1}
 	})
 	food.craft({
-		output = "food_sweet:rhubarb 4",
+		output = "food_sweet:rhubarb 10",
 		recipe = {
 			{"wool:red"},
 			{"wool:red"},
 			{"wool:red"}
+		}
+	})
+end)
+food.asupport("lemon",function()
+	minetest.register_craftitem("food_sweet:lemon", {
+		description = S("Lemon"),
+		inventory_image = "food_lemon.png",
+		groups = {food_lemon = 1}
+	})
+	food.craft({
+		output = "food_sweet:lemon 4",
+		recipe = {
+			{"","default:sand",""},
+			{"default:sand","","default:sand"},
+			{"","default:sand",""}
 		}
 	})
 end)
@@ -142,7 +161,7 @@ minetest.register_craftitem("food_sweet:cake_cheese",{
 	description = S("Strawberry Cheese Cake"),
 	inventory_image = "food_sweet_cake_cheese.png",
 	on_use = food.item_eat(4),
-	groups={food=3,crumbly=3}
+	groups={food=3}
 })
 minetest.register_craftitem("food_sweet:cakemix_cheese",{
 	description = S("Strawberry Cheese Cake Mix"),
@@ -167,7 +186,7 @@ minetest.register_craftitem("food_sweet:cake_cheese_blueberry",{
 	description = S("Blueberry Cheese Cake"),
 	inventory_image = "food_sweet_cake_cheese_blueberry.png",
 	on_use = food.item_eat(4),
-	groups={food=3,crumbly=3}
+	groups={food=3}
 })
 minetest.register_craftitem("food_sweet:cakemix_cheese_blueberry",{
 	description = S("Blueberry Cheese Cake Mix"),
@@ -192,7 +211,7 @@ minetest.register_craftitem("food_sweet:cake_triple_choco",{
 	description = S("Triple Chocolate Cake"),
 	inventory_image = "food_sweet_cake_triple_choco.png",
 	on_use = food.item_eat(4),
-	groups={food=3,crumbly=3}
+	groups={food=3}
 })
 minetest.register_craftitem("food_sweet:cakemix_triple_choco",{
 	description = S("Triple Chocolate Cake Mix"),
@@ -217,7 +236,7 @@ minetest.register_craftitem("food_sweet:cake_wedding",{
 	description = S("Wedding Cake"),
 	inventory_image = "food_sweet_cake_wedding.png",
 	on_use = food.item_eat(4),
-	groups={food=3,crumbly=3}
+	groups={food=3}
 })
 
 food.craft({
@@ -234,7 +253,7 @@ minetest.register_craftitem("food_sweet:crumble_rhubarb",{
 	description = S("Rhubarb Crumble"),
 	inventory_image = "food_sweet_crumble_rhubarb.png",
 	on_use = food.item_eat(4),
-	groups={food=3,crumbly=3}
+	groups={food=3}
 })
 minetest.register_craftitem("food_sweet:crumble_rhubarb_raw",{
 	description = S("Raw Rhubarb Crumble"),
@@ -251,5 +270,136 @@ food.craft({
 	recipe = {
 		{"","group:food_rhubarb",""},
 		{"group:food_flour","group:food_sugar","group:food_egg"}
+	}
+})
+
+-- Cup cake
+minetest.register_craftitem("food_sweet:cupcake",{
+	description = S("Cupcake"),
+	inventory_image = "food_sweet_cupcake.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+minetest.register_craftitem("food_sweet:cupcake_mix",{
+	description = S("Cupcake Mix"),
+	inventory_image = "food_sweet_cupcake_mix.png"
+})
+food.craft({
+	type = "cooking",
+	output = "food_sweet:cupcake",
+	recipe = "food_sweet:cupcake_mix",
+	cooktime = 1
+})
+food.craft({
+	output = "food_sweet:cupcake_mix 6",
+	recipe = {
+		{"group:food_flour","","group:food_egg"},
+		{"","group:food_sugar",""}
+	}
+})
+
+-- Chocolate Cupcake
+minetest.register_craftitem("food_sweet:cupcake_choco",{
+	description = S("Chocolate Cupcake"),
+	inventory_image = "food_sweet_cupcake_choco.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+minetest.register_craftitem("food_sweet:cupcake_choco_mix",{
+	description = S("Chocolate Cupcake Mix"),
+	inventory_image = "food_sweet_cupcake_choco_mix.png"
+})
+food.craft({
+	type = "cooking",
+	output = "food_sweet:cupcake_choco",
+	recipe = "food_sweet:cupcake_choco_mix",
+	cooktime = 1
+})
+food.craft({
+	output = "food_sweet:cupcake_choco_mix 6",
+	recipe = {
+		{"group:food_flour","group:food_milk_chocolate","group:food_egg"},
+		{"","group:food_sugar",""}
+	}
+})
+
+-- Fairy Cake
+minetest.register_craftitem("food_sweet:cupcake_fairy",{
+	description = S("Fairy Cake"),
+	inventory_image = "food_sweet_cupcake_fairy.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+food.craft({
+	output = "food_sweet:cupcake_fairy",
+	recipe = {
+		{"group:food_sugar"},
+		{"food_sweet:cupcake"}
+	}
+})
+
+-- Lemon Cake
+minetest.register_craftitem("food_sweet:cupcake_lemon",{
+	description = S("Lemon Drizzle Cupcake"),
+	inventory_image = "food_sweet_cupcake_lemon.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+food.craft({
+	output = "food_sweet:cupcake_lemon",
+	recipe = {
+		{"group:food_lemon"},
+		{"food_sweet:cupcake"}
+	}
+})
+
+-- Blueberry Muffin
+minetest.register_craftitem("food_sweet:muffin_blueberry",{
+	description = S("Blueberry Muffin"),
+	inventory_image = "food_sweet_muffin_blueberry.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+minetest.register_craftitem("food_sweet:muffin_blueberry_mix",{
+	description = S("Blueberry Muffin Mix"),
+	inventory_image = "food_sweet_cupcake_mix.png"
+})
+food.craft({
+	type = "cooking",
+	output = "food_sweet:muffin_blueberry",
+	recipe = "food_sweet:muffin_blueberry_mix",
+	cooktime = 1
+})
+food.craft({
+	output = "food_sweet:muffin_blueberry_mix 6",
+	recipe = {
+		{"group:food_flour","group:food_blueberry","group:food_egg"},
+		{"","group:food_sugar",""}
+	}
+})
+
+-- Chocolate Chip Muffin
+minetest.register_craftitem("food_sweet:muffin_choco",{
+	description = S("Chocolate Chip Muffin"),
+	inventory_image = "food_sweet_muffin_choco.png",
+	on_use = food.item_eat(2),
+	groups={food=1}
+})
+minetest.register_craftitem("food_sweet:muffin_choco_mix",{
+	description = S("Chocolate Chip Muffin Mix"),
+	inventory_image = "food_sweet_cupcake_choco_mix.png"
+})
+food.craft({
+	type = "cooking",
+	output = "food_sweet:muffin_choco",
+	recipe = "food_sweet:muffin_choco_mix",
+	cooktime = 1
+})
+food.craft({
+	output = "food_sweet:muffin_choco_mix 6",
+	recipe = {
+		{"","group:food_milk_chocolate",""},
+		{"group:food_flour","group:food_milk_chocolate","group:food_egg"},
+		{"","group:food_sugar",""}
 	}
 })
